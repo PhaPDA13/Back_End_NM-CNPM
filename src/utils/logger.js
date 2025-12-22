e392f4b99f1f26afe06a08af33c8b2047ccd3a31
hcmus-phat <23120317@student.hcmus.edu.vn>
2025-12-22 15:32:53 +0700

    fix: add logs folder

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -13,8 +13,8 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/app.log" }),
+    new winston.transports.File({ filename: "../logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "../logs/app.log" }),
   ],
 });
 
